dc5f1963122b5e71533a00c5768a6a7a4e13ec2e
90NA001VI <citarellagaetano26@gmail.com>
2020-11-10 22:06:16 +0100

    Calcolatrice 0.2.5

--- Calcolatrice.py
+++ Calcolatrice.py
@@ -3,8 +3,6 @@ from tkinter import ttk, Canvas
 from tkinter import filedialog
 import os
 
-
-
 def open_program_somma():
     os.system('"%s"' %"single programs\somma-sottrazione.py")
 
@@ -50,8 +48,8 @@ def open_program_about():
 def open_program_forze():
     os.system('"%s"' %"single programs\orzef.py")
 
-def open_program_tempo():
-    os.system('"%s"' %"single programs\empot.py")
+def open_program_forze2():
+    os.system('"%s"' %"single programs\orzef2.py")
 
 def donothing():
    filewin = Toplevel(root)
@@ -86,9 +84,8 @@ filemenu2.add_command(label="Cramer3x3", command=open_program_sistema3x3)
 filemenu3 = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Fisica", menu=filemenu3)
 filemenu3.add_command(label="Equivalenze", command=open_program_equivalenze)
-filemenu3.add_command(label="Conversioni Tempo", command=open_program_tempo)
 filemenu3.add_command(label="Forze", command=open_program_forze)
-filemenu3.add_command(label="Forze 2", command=donothing)
+filemenu3.add_command(label="Forze 2", command=open_program_forze2)
 filemenu3.add_command(label="Momenti Forze", command=donothing)
 
 
