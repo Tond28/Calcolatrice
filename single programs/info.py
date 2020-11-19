cd1f1288d8ce27d74d77dd97425222318de7fe1a
Gaetandroid <73991618+Gaetandroid@users.noreply.github.com>
2020-11-19 14:15:24 +0100

    aggiornamento info.py 0.2.9

--- single programs/info.py
+++ single programs/info.py
@@ -8,7 +8,6 @@ root.geometry("600x500")
 mainframe = ttk.Frame(root)
 mainframe.place(height=0, x=0, y=0)
 
-
 Label(root, text="Calcolatrice creata da: Gaetano Citarella, Filippo Tondelli", font=('verdana', 10)).place(height=20, x=0, y=0)
 Label(root, text="Versione programma: 0.2.9", font=('verdana', 10)).place(height=20, x=0, y=25)
 Label(root, text="Questo programma è stato ideato per fare calcoli di: \nMatematica di Base, Algebra, Fisica, Geometria, Geometria Solida.", font=('verdana', 10)).place(height=40, x=1, y=45)
