721ea4412b0a185367f0b6366f6fcc66f2de4013
90NA001VI <citarellagaetano26@gmail.com>
2020-11-20 21:46:51 +0100

    Aggiornamento 0.3.1

--- single programs/info.py
+++ single programs/info.py
@@ -9,7 +9,7 @@ mainframe = ttk.Frame(root)
 mainframe.place(height=0, x=0, y=0)
 
 Label(root, text="Calcolatrice creata da: Gaetano Citarella, Filippo Tondelli", font=('verdana', 10)).place(height=20, x=0, y=0)
-Label(root, text="Versione programma: 0.2.9", font=('verdana', 10)).place(height=20, x=0, y=25)
+Label(root, text="Versione programma: 0.3.1", font=('verdana', 10)).place(height=20, x=0, y=25)
 Label(root, text="Questo programma è stato ideato per fare calcoli di: \nMatematica di Base, Algebra, Fisica, Geometria, Geometria Solida.", font=('verdana', 10)).place(height=40, x=1, y=45)
 Label(root, text="^ATTUALMENTE IN AGGIORNAMENTO^", font=('verdana', 10)).place(height=20, x=165, y=90)
 
