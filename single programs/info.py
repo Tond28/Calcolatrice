8a48a7efaeabe79d9af08a295d2f01bb028fbece
90NA001VI <citarellagaetano26@gmail.com>
2020-11-22 19:50:10 +0100

    info fix 0.4.4

--- single programs/info.py
+++ single programs/info.py
@@ -8,9 +8,21 @@ root.geometry("600x500")
 mainframe = ttk.Frame(root)
 mainframe.place(height=0, x=0, y=0)
 
-Label(root, text="Calcolatrice creata da: Gaetano Citarella, Filippo Tondelli", font=('verdana', 10)).place(height=20, x=0, y=0)
-Label(root, text="Versione programma: 0.3.1", font=('verdana', 10)).place(height=20, x=0, y=25)
-Label(root, text="Questo programma è stato ideato per fare calcoli di: \nMatematica di Base, Algebra, Fisica, Geometria, Geometria Solida.", font=('verdana', 10)).place(height=40, x=1, y=45)
-Label(root, text="^ATTUALMENTE IN AGGIORNAMENTO^", font=('verdana', 10)).place(height=20, x=165, y=90)
+Label(root, text="Calcolatrice creata da: Gaetano Citarella, Filippo Tondelli", font=('verdana', 15)).place(height=25, x=0, y=0)
+Label(root, text="Questo programma è stato ideato per fare calcoli di: \nMatematica di Base, Algebra, Fisica, Geometria, Geometria Solida.", font=('verdana', 10)).place(height=40, x=70, y=30)
+Label(root, text="^ATTUALMENTE IN AGGIORNAMENTO^", font=('verdana', 10)).place(height=20, x=165, y=70)
+
+frames = [PhotoImage(file='images/spalla2.gif',format = 'gif -index %i' %(i)) for i in range(40)]
+def update(ind):
+    frame = frames[ind]
+    ind += 1
+    if ind>39:
+        ind = 0
+    label.configure(image=frame)
+    root.after(100, update, ind)
+
+label = Label(root)
+label.place(height=500, x=230, y=90)
+root.after(0, update, 0)
 
 root.mainloop()
